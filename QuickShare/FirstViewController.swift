f05beeca899d1643018a39a9b03bd0952d077264
Nick Ludlow <nickludlow82@gmail.com>
2015-06-05 14:59:53 +0100

    Initial Commit

---
+++ QuickShare/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuickShare
+//
+//  Created by Nick Ludlow on 05/06/2015.
+//  Copyright (c) 2015 Nick Ludlow. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuickShare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QuickShare
+//
+//  Created by Nick Ludlow on 05/06/2015.
+//  Copyright (c) 2015 Nick Ludlow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
